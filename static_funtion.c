8c165436c9b3ebbb49a3453d41dfe772f0d82e68
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2023-10-26 00:15:08 +0700

    compilation error in static function

--- static_funtion.c
+++ static_funtion.c
@@ -1,9 +1,9 @@
 #include "static_funtion.h"
-static int cong1(int a, int b)
+static int cong(int a, int b)
 {
     return a - b;
 }
 int tru(int a, int b)
 {
-    return cong1(a, b);
+    return cong(a, b);
 }
--- static_funtion.h
+++ static_funtion.h
@@ -1,6 +1,6 @@
 #ifndef static_funtion
 #define static_funtion
 
-static int cong1(int, int);
+static int cong(int, int);
 int tru(int, int);
 #endif
