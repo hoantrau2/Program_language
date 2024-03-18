35b21899d89d24caed87605ad482fab2b78c873e
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2024-03-18 13:22:56 +0700

    Hello world

--- code_c/main.c
+++ code_c/main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
-// #include "static_funtion.h"
-// #include "non_static_funtion.h"
 
 int main() {
-  int a = 5;
-  int b = 7;
-
-  printf("       a = %d, b= %d", a, b);
-
+  printf("Hello world \n");
   return 0;
 }
-
-
-// declare static funtion before static global
-// cong = %d, tru =%d \n", a, b, cong(a, b), tru(a, b));
--- code_c/non_static_funtion.c
+++
@@ -1,7 +0,0 @@
-
-#include "non_static_funtion.h"
-
-int cong(int a, int b)
-{
-    return a + b;
-}
