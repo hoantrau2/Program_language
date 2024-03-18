5e1b9ddebbc064ffae2c188d857ce249b952851f
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2024-03-18 13:18:01 +0700

    static library

--- code_c/main.c
+++ code_c/main.c
@@ -11,9 +11,6 @@ int main() {
   return 0;
 }
 
-// gcc -c static_funtion.c -o static_funtion.o
-// gcc -c non_static_funtion.c -o non_static_funtion.o
-// gcc main.c static_funtion.o non_static_funtion.o -o main
-// ./main
+
 // declare static funtion before static global
 // cong = %d, tru =%d \n", a, b, cong(a, b), tru(a, b));
---
+++ note.c
@@ -0,0 +1,4 @@
+// gcc -c static_funtion.c -o static_funtion.o
+// gcc -c non_static_funtion.c -o non_static_funtion.o
+// gcc main.c static_funtion.o non_static_funtion.o -o main
+// ./main
