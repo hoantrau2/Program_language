8720dbe29703500f417c85836158c0590abb82a4
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2024-03-20 00:43:19 +0700

    funtion in C++

--- code_cpp/first_code.cpp
+++ code_cpp/first_code.cpp
@@ -1,20 +1,16 @@
-//Variables in C++
+// Functions in C++
+// when getting to classes, functions that is called blocks before called
+// methods. funtions are not part of a class
 #include <iostream>
 
+int multiply(int a, int b) { return a * b; }
+void multiplyAndlog(int a, int b) {
+  int result = multiply(a, b);
+  std::cout << result << std::endl;
+}
 int main() {
-  short a = 'A';
-  char aa = 'A';
-  std::cout << a << std::endl;
-  std::cout << aa << std::endl;
-  std::cout << "bool type is 1 byte" << std::endl;
-  std::cout << "Size of bool: " << sizeof(bool) << " bytes" << std::endl;
-  std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
-  std::cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
-  std::cout << "Size of long: " << sizeof(long) << " bytes" << std::endl;
-  std::cout << "Size of long long : " << sizeof(long long) << " bytes"
-            << std::endl;
-  // declaring variables with type char, the default is character, other types
-  // are numbers
-  //  bool type is 1 byte in size
+
+  multiplyAndlog(2, 3);
+  multiplyAndlog(56, 78);
   return 0;
 }
