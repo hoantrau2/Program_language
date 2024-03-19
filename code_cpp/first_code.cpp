dcb221d7021352dd90a78624b8099dd956b26154
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2024-03-19 23:39:40 +0700

    namespace

--- code_cpp/first_code.cpp
+++ code_cpp/first_code.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // this library stands for input and output stream, this alows get input from the console or output something on the console
+using namespace std; //"std" stands for standard -> inside of iostream, there is
+                     // a namespace is defined call std and inisde of that
+                     // namespace, have all funtion like "cout"
 int main() {
   cout << "hello world" << endl;
   return 0;
 }
+
+
+
+// namespace is just a collection of code. It's a way to divide code up into
+// specific areas. So, one code related to one thing is going to be a one
+// namespace. One code related to another thing is going to be a another
+// namespace
